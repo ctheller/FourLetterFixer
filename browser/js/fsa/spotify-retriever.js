5fb0cdec8d56f4bf1ea7ca038abaf77e1973f725
Chris Heller <ctheller12@gmail.com>
2016-07-21 17:18:31 -0700

    Clean Button Works

--- browser/js/fsa/spotify-retriever.js
+++ browser/js/fsa/spotify-retriever.js
@@ -7,6 +7,7 @@ app.factory('SpotifyRetriever', function(AuthService, Spotify, $log){
     //     })
     // }
 
+    //Gets User Info
 	this.getUserDetails = function(){
     	return Spotify.getCurrentUser()
     	.then(function(userDetails){
@@ -15,6 +16,7 @@ app.factory('SpotifyRetriever', function(AuthService, Spotify, $log){
         .catch($log);   
 	}
 
+    //Retrieves ALL of a user's public, private, and followed playlists
     this.getAllPlaylists = function(id){
         var recWrap = function(i){
             return Spotify.getUserPlaylists(id, {limit:50, offset:i})
@@ -32,6 +34,7 @@ app.factory('SpotifyRetriever', function(AuthService, Spotify, $log){
         return recWrap(0);
     }
 
+    //Retrieves ALL songs from a given playlist
     this.getPlaylistSongs = function(userId, playlistId){
         var recWrap = function(i){
             return Spotify.getPlaylistTracks(userId, playlistId, {market:"US", offset:i})
@@ -52,6 +55,25 @@ app.factory('SpotifyRetriever', function(AuthService, Spotify, $log){
         }
         return recWrap(0);
     }
+
+    //Creates a new playlist without the explicit tracks
+    this.makeCleanPlaylist = function(userId, name, songs){
+        var cleanSongArray = songs.filter(function(song){
+            return !song.explicit;
+        })
+        var cleanSongIdArray = cleanSongArray.map(function(song){
+            return song.id;
+        })
+        Spotify.createPlaylist(userId, {name: name+" (Clean)", public: false})
+        .then(function(createdPlaylist){
+            Spotify.addPlaylistTracks(userId,createdPlaylist.id,cleanSongIdArray);
+        })
+    }
+
+    // Spotify.removePlaylistTracks('user_id', 'playlist_id', 'comma separated string or array of spotify track ids or uris');
+    // Spotify.addPlaylistTracks('user_id', 'playlist_id', 'comma separated string or array of spotify track uris');
+    // Spotify.createPlaylist('user_id', {name:'', public: false});
+
 
     return this;
 })
--- browser/js/playlist/playlist.js
+++ browser/js/playlist/playlist.js
@@ -1,39 +1,19 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('playlist', {
-        url: '/playlist/:ownerId/:playlistId',
+        url: '/playlist/:ownerId/:playlistId/:playlistName',
         templateUrl: 'js/playlist/playlist.html'
     });
 });
 
 app.controller('playlist', function ($rootScope, $scope, SpotifyRetriever, Spotify, $stateParams) {
-    // SpotifyRetriever.promiseForToken()
-    // .then(function(){
-    // 	return SpotifyRetriever.getUserDetails()
-    // })
- //    SpotifyRetriever.getUserDetails()
- //    .then(function(userDetails){
- //        $rootScope.user = userDetails;
- //    })
- //    .then(function(){
-	//     return SpotifyRetriever.getAllPlaylists($scope.user.id)
-	// })
- //    .then(function(playlists){
- //    	return $scope.playlists = playlists;
- //    })
-    // .then(function(playlists){
-    //     return SpotifyRetriever.getPlaylistSongs($scope.user.id, $scope.playlists[2].id);
-    // })
+
     SpotifyRetriever.getPlaylistSongs($stateParams.ownerId, $stateParams.playlistId)
     .then(function(songs){
         $scope.songs = songs;
         return songs;
     })
 
-    // Spotify.getTrack('0eGsygTp906u18L0Oimnem').then(function (data) {
-    //     console.log("Get Track:",data);
-    // });
-
-    // Spotify.getTrackAudioFeatures('0eGsygTp906u18L0Oimnem').then(function (data) {
-    //     console.log("Get Features:",data);
-    // });
+    $scope.makeCleanPlaylist = function(){
+        SpotifyRetriever.makeCleanPlaylist($scope.user.id, $stateParams.playlistName, $scope.songs);
+    }
 });
